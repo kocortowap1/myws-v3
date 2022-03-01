f354d66b0a8e1ea297c1a9a3041687e7962856f6
Anshari <kocortowap1@gmail.com>
2022-03-01 20:18:15 +0700

    set layout

---
+++ src/layouts/AdminLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <divc class="flex bg-gray-100">
+    <!-- sidebar -->
+    <sidebar />
+    <div class="container px-5 py-5">
+      <router-view />
+    </div>
+  </divc>
+</template>
+<script>
+import sidebar from "../components/Sidebar.vue";
+export default {
+  name: "AdminLayout",
+  components: {
+    sidebar,
+  },
+};
+</script>
