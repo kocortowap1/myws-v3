352d043b04fc9e537ab1f24d3c0a2834735eb14c
Anshari <kocortowap1@gmail.com>
2022-03-01 21:28:55 +0700

    set layout content

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,8 +1,8 @@
 <template>
-  <divc class="flex bg-gray-100">
+  <divc class="min-w-full flex bg-gray-100">
     <!-- sidebar -->
     <sidebar />
-    <div class="container px-5 py-5">
+    <div class="container px-5 py-5 mx-auto">
       <router-view />
     </div>
   </divc>
