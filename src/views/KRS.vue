6b922f08db649363ab15b687d4f3c3b0b6239929
Anshari <kocortowap1@gmail.com>
2022-03-01 21:29:12 +0700

    minor update

--- src/views/KRS.vue
+++ src/views/KRS.vue
@@ -1,17 +1,19 @@
 <template>
+  <div>
+    <h1 class="text-2xl font-bold">KRS & NILAI</h1>
     <list-krs />
+  </div>
 </template>
 
 <script>
-import ListKrs from '../components/krs/list.vue'
+import ListKrs from "../components/krs/list.vue";
 export default {
-    name :'KRS',
-    components: {
-        ListKrs
-    }
-}
+  name: "KRS",
+  components: {
+    ListKrs,
+  },
+};
 </script>
 
 <style>
-
 </style>
