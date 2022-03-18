3200c030885e1292108f5d3f2b206f417d2f293a
Anshari <kocortowap1@gmail.com>
2022-03-19 00:22:54 +0700

    remove unused code

--- src/composable/dictionary.js
+++ src/composable/dictionary.js
@@ -1,22 +1,16 @@
 import { getDictionary } from "../lib/pddikti"
 
 export function useDictionary() {
-    // async function getDictionary(fungsi) {
-    //     const search = await getData({ act: 'GetDictionary', fungsi: fungsi })
-    // }
+
     async function parseRequest(fungsi) {
         let requestData = []
         const search = await getDictionary(fungsi)
         if (search.status) {
-            let requestData = []
             for (const key in search['data']['request']) {
                 if (key !== "error_code" && key !== "error_desc" && key !== "token") {
                     const pecah = key.split("[")
-                    // if(pecah[0]=== "data" || pecah[0] === "Data"){
                     requestData.push({ fieldname: pecah[1].substring(0, pecah[1].length - 1), ...search['data']['request'][key] })
-                    // }else{
 
-                    // }
                 }
             }
             return requestData
@@ -25,5 +19,5 @@ export function useDictionary() {
         }
 
     }
-    return { getDictionary, parseRequest }
+    return { parseRequest }
 }
