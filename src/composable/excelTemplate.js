11e5d86ab6a7fba4dd2258bdfe96ac9a71260685
Anshari <kocortowap1@gmail.com>
2022-03-21 23:12:27 +0700

    create export template

---
+++ src/composable/excelTemplate.js
@@ -0,0 +1,21 @@
+import { useDictionary } from './dictionary.js';
+import { useExcel } from './excel'
+export function useExcelTemplate() {
+    const { parseRequest } = useDictionary();
+    const { writeExcel } = useExcel()
+    const downloadTemplate = async (fungsi) => {
+        const d = await parseRequest(fungsi);
+        if (d['status'] !== false) {
+            let data = []
+            d.forEach(el => {
+                // console.log(el['fieldname'])
+                data.push({ [el['fieldname']]: el['keterangan'] })
+            })
+            console.log(data)
+            await writeExcel({ filename: `${fungsi}.xlsx`, data: data })
+        } else {
+            alert(d.message)
+        }
+    }
+    return { downloadTemplate }
+}
