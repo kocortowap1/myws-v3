c567e89a82bce6bcacb23d331baaa42116839a0d
Anshari <kocortowap1@gmail.com>
2022-03-24 22:21:38 +0700

    enable composable devtools

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './index.css'
+import { VueComposableDevtools } from 'vue-composable'
 
 createApp(App)
     .use(router)
+    .use(VueComposableDevtools)
     .mount('#app')
