d03f3c038fa41cbed731c4b22f57079ff3adf04f
Anshari <kocortowap1@gmail.com>
2022-04-04 04:06:50 +0700

    remove unused code

--- src/views/MHS.vue
+++ src/views/MHS.vue
@@ -4,10 +4,7 @@
       <h1 class="text-2xl font-bold">Mahasiswa</h1>
       <div class="space-x-2">
         <a class="btn btn-primary text-white" href="/mhs/import">Import</a>
-        <!-- <a class="btn btn-accent text-white" @click="createTemplate"
-          >Template</a
-        > -->
-        <!-- href="./assets/xlsx/matakuliah.xlsx" -->
+
       </div>
     </header>
   </div>
