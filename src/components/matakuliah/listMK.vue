4f4f5263b674d4a6e359ce1661a3ed84dd1724bc
Anshari <kocortowap1@gmail.com>
2022-04-04 04:06:35 +0700

    change actions

--- src/components/matakuliah/listMK.vue
+++ src/components/matakuliah/listMK.vue
@@ -39,40 +39,45 @@
     <tbody>
       <tr v-for="(mk, i) in listMK" :key="i">
         <td>
-          <div class="btn-group btn-group-xs">
-            <a :href="`/matakuliah/form/${mk.id_matkul}`" class="btn btn-xs btn-primary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                />
-              </svg>
-            </a>
-            <button class="btn btn-xs btn-secondary">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-            </button>
-          </div>
+          <div class="dropdown">
+                <label tabindex="0" class="btn btn-xs btn-default">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabindex="0"
+                  class="
+                    dropdown-content
+                    menu
+                    p-2
+                    shadow
+                    bg-base-100
+                    rounded-box
+                    w-52
+                  "
+                >
+                  <li><a :href="`/matakuliah/form/${mk.id_matkul}`">Edit</a></li>
+                  <li><a>Delete</a></li>
+                </ul>
+              </div>
+
         </td>
         <td>{{ mk.nama_program_studi }}</td>
         <td>{{ mk.kode_mata_kuliah }}</td>
