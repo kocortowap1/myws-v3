d844d44fbacc59f51ea725f2a4514b723a15eb3a
Anshari <kocortowap1@gmail.com>
2022-04-04 04:06:12 +0700

    using suspense

--- src/components/kelas/listKelas.vue
+++ src/components/kelas/listKelas.vue
@@ -1,21 +1,91 @@
 <template>
-  
+  <div class="bg-white px-4 py-2">
+      <div v-if="error">{{error}}</div>
+    <div class="overlow-auto">
+      <table class="table table-compact w-full">
+        <thead>
+          <tr>
+            <th></th>
+            <th>Prodi</th>
+            <th>Kode MK</th>
+            <th>Matakuliah</th>
+            <th>Kelas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(k, i) in list" :key="i">
+            <td>
+              <div class="dropdown">
+                <label tabindex="0" class="btn btn-xs btn-default">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-4 w-4"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </label>
+                <ul
+                  tabindex="0"
+                  class="
+                    dropdown-content
+                    menu
+                    p-2
+                    shadow
+                    bg-base-100
+                    rounded-box
+                    w-52
+                  "
+                >
+                  <li><a :href="`/kelas/form/${k.id_kelas_kuliah}`">Edit</a></li>
+                  <li><a>Delete</a></li>
+                </ul>
+              </div>
+            </td>
+            <td>{{ k.nama_program_studi }}</td>
+            <td>{{ k.kode_mata_kuliah }}</td>
+            <td>{{ k.nama_mata_kuliah }}</td>
+            <td>{{ k.nama_kelas_kuliah }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import { reactive } from '@vue/reactivity'
+import { reactive, ref } from "@vue/reactivity";
+import { useKelasKuliah } from "../../composable/kelasKuliah";
 export default {
-    name: 'listKelas',
-    setup(){
-        const listKelas = reactive([])
-        
-        return {
-
-        }
+  name: "listKelas",
+  async setup() {
+    const list = reactive([]);
+    const error = ref('')
+    const requestParams = reactive({})
+    const { getListKelasKuliah } = useKelasKuliah();
+    const prepare = await getListKelasKuliah({});
+    if (prepare["status"] === undefined) {
+      for (const l of prepare) {
+        list.push(l);
+      }
+    }else{
+        error.value = prepare['message']
     }
-}
+    return {
+      list,
+    };
+  },
+};
 </script>
-
-<style>
-
-</style>
--- src/views/Kelas.vue
+++ src/views/Kelas.vue
@@ -1,39 +1,35 @@
 <template>
-  <div>
+  <div class="mb-4">
     <header class="flex items-center justify-between">
       <h1 class="text-2xl font-bold">Kelas Kuliah</h1>
       <a class="btn btn-primary text-white" href="/kelas/import">Import</a>
     </header>
   </div>
-  <div>
-    <list-kelas></list-kelas>
-  </div>
-  <!-- <div class="flex flex-col justify-center items-center">
-    <span class="text-3xl">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-32 w-32"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        stroke-width="2"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-        />
-      </svg>
-    </span>
-    <span class="text-4xl"> Fitur Kelas sedang diperbaiki </span>
-  </div> -->
+  <Suspense>
+    <template #default>
+      <list-kelas />
+    </template>
+    <template #fallback> Loading data.... </template>
+  </Suspense>
 </template>
 <script>
-import listKelas from "../components/kelas/listKelas.vue";
+import { defineAsyncComponent, ref, onErrorCaptured } from "vue";
+const listKelas = defineAsyncComponent(() =>
+  import("../components/kelas/listKelas.vue")
+);
 export default {
   name: "Kelas",
   components: {
-    listKelas,
+    "list-kelas": listKelas,
+  },
+  setup() {
+    let errorMessage = ref("");
+    onErrorCaptured((e) => {
+      errorMessage.value = e;
+    });
+    return {
+      errorMessage,
+    };
   },
 };
 </script>
